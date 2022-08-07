e8fb2222179f196333e1ed4248a735cf86cbe45d
ph04 <tacc010203@gmail.com>
2022-08-07 14:41:34 +0200

    Sudoku solver completed

--- src/sudoku.rs
+++ src/sudoku.rs
@@ -1,9 +1,12 @@
+/// Represents a sudoku grid.
+#[derive(Debug, Clone, Copy)]
 pub struct Sudoku {
     grid: [u8; 81],
     is_solved: bool,
 }
 
 impl Sudoku {
+    /// Checks if the given number can be put in the given row.
     fn is_row_valid(&self, number: u8, row_index: usize) -> bool {
         !self.grid
             .chunks_exact(9)
@@ -13,6 +16,7 @@ impl Sudoku {
             .any(|n| *n == number)
     }
 
+    /// Checks if the given number can be put in the given column.
     fn is_column_valid(&self, number: u8, col_index: usize) -> bool {
         !self.grid
             .chunks_exact(9)
@@ -20,24 +24,31 @@ impl Sudoku {
             .any(|n| n == number)
     }
 
+    /// Checks if the given number can be put in the given cell.
     fn is_cell_valid(&self, number: u8, (x, y): (usize, usize)) -> bool {
         !self.grid
             .chunks_exact(27)
             .nth(y / 3)
             .unwrap()
             .chunks_exact(9)
-            .nth(x / 3)
-            .unwrap()
-            .iter()
-            .any(|n| *n == number)
+            .any(|row| {
+                row
+                    .chunks_exact(3)
+                    .nth(x / 3)
+                    .unwrap()
+                    .iter()
+                    .any(|n| *n == number)
+            })
     }
 
+    /// Checks if the given number can be put in the given position.
     fn is_valid(&self, number: u8, (x, y): (usize, usize)) -> bool {
         self.is_row_valid(number, y) && 
         self.is_column_valid(number, x) &&
         self.is_cell_valid(number, (x, y))
     }
 
+    /// Transforms the given index into a coordinates pair.
     fn get_coordinates(mut curr_idx: usize) -> (usize, usize) {
         let mut curr_y = 0;
 
@@ -50,16 +61,12 @@ impl Sudoku {
         (curr_idx, curr_y)
     }
 
+    /// Recursively solves the sudoku through backtracking,
+    /// starting from the given index.
     fn solve_internals(&mut self, curr_idx: usize) {
-        println!("{}", self);
-
-        if self.is_solved {
-            return
-        }
-
         if curr_idx == 81 {
             self.is_solved = true;
-            return
+            return;
         }
 
         if self.grid[curr_idx] != 0 {
@@ -70,6 +77,10 @@ impl Sudoku {
                     self.grid[curr_idx] = n;
 
                     self.solve_internals(curr_idx + 1);
+
+                    if self.is_solved {
+                        return;
+                    }
                 }
             }
 
@@ -77,8 +88,32 @@ impl Sudoku {
         }
     }
 
+    /// Solves the sudoku by using recursion and backtracking.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use sudoku::sudoku::Sudoku;
+    /// let mut sudoku = Sudoku::default(); // empty grid
+    /// 
+    /// sudoku.solve() // the sudoku is now solved!
+    /// ```
     pub fn solve(&mut self) {
         self.solve_internals(0);
+    }
+
+    /// Returns the current state of the sudoku.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use sudoku::sudoku::Sudoku;
+    /// let sudoku = Sudoku::default(); // blank grid
+    /// 
+    /// assert_eq!(sudoku.grid(), [0; 81]);
+    /// ```
+    pub fn grid(&self) -> [u8; 81] {
+        self.grid
     }
 }
 
@@ -89,7 +124,26 @@ impl TryFrom<[u8; 81]> for Sudoku {
         if grid.iter().any(|n| *n > 9) {
             Err(SudokuError::InvalidCell)
         } else {
-            Ok(Self { grid, is_solved: false })
+            Ok(Self {
+                grid,
+                is_solved: false
+            })
+        }
+    }
+}
+
+impl std::cmp::PartialEq<[u8; 81]> for Sudoku {
+    fn eq(&self, other: &[u8; 81]) -> bool {
+        self.grid() == *other
+    }
+}
+
+impl Default for Sudoku {
+    /// Returns an empty grid.
+    fn default() -> Self {
+        Self {
+            grid: [0; 81],
+            is_solved: false,
         }
     }
 }
@@ -102,7 +156,7 @@ impl std::fmt::Display for Sudoku {
                 .map(|row| {
                     row
                         .iter()
-                        .map(|n| n.to_string())
+                        .map(|n| if *n != 0 { n.to_string() } else { " ".to_owned()})
                         .intersperse(' '.to_string())
                         .collect::<String>()
                 })
